3b15f3838f7c15cecb6e9dfd8e2a24c54a4bf73f
owewhe23 <calan.g.mcdermott@gmail.com>
2022-07-23 10:55:30 +1200

    Code Simplification

--- Player.gd
+++ Player.gd
@@ -47,35 +47,22 @@ func _physics_process(delta):
 	if is_on_floor():
 		print("on floor")
 		velocity.y = 0
+		player_state = state.IDLE
 	elif is_on_floor() == false:
 		print("not on floor")
-	#if Input.is_action_pressed(right_key):
-	#	print("right")
-	move_dir = Input.get_action_strength(right_key) - Input.get_action_strength(left_key)
-	if move_dir != 0:
-		print("moving")
-		velocity.x = move_dir*speed
-	else: 
-		velocity.x = 0
-	if Input.is_action_pressed(up_key) and is_on_floor():
-		velocity.y += jump_speed
-		
-	
-	if is_on_floor():
-		player_state = state.IDLE
-	elif not is_on_floor():
 		#print("not on floor")
 		if velocity.y <= 0:
 			player_state = state.JUMP
+			JUMP()
 		else:
 			player_state = state.FALL
-	
-	if player_state == state.FALL:
-		#print("falling")
-		FALL()
-	elif player_state == state.JUMP:
-		#print("jumping")
-		JUMP()
+			FALL()
+	move_dir = Input.get_action_strength(right_key) - Input.get_action_strength(left_key)
+	if move_dir:
+		print("moving")
+	velocity.x = move_dir*speed
+	if Input.is_action_pressed(up_key) and is_on_floor():
+		velocity.y += jump_speed
 	move_and_slide(velocity, Vector2.UP)
 	
 
