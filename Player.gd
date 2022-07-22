52cd9b05fa222eac6837953991efdd4157544562
owewhe23 <82478297+Yagoi23@users.noreply.github.com>
2022-07-23 10:39:34 +1200

    Updated player code
    left and right movement
    bug* doesn't touch floor

--- Player.gd
+++ Player.gd
@@ -6,9 +6,12 @@ extends KinematicBody2D
 # var b = "text"
 export var player_num := 0
 
-export (int) var speed = 120
+export (int) var speed = 150
+export (int) var jump_speed = -150
 export (float) var gravity = 9.8
 export (float) var weight = 1
+#export (float,0,1,0) var acceleration = 1
+#export (int) var friction = 0
 
 var velocity = Vector2.ZERO
 
@@ -26,6 +29,7 @@ var up_key
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player_state = state.IDLE
 	if player_num == 1:
 		sprite.color = Color(255,255,255)
 		right_key = "p2_right"
@@ -35,11 +39,27 @@ func _ready():
 		right_key = "p1_right"
 		left_key = "p1_left"
 		up_key = "p1_up"
-
-func get_input():
-	move_dir = Input.get_action_strength(right_key) - Input.get_action_strength(left_key)
-
+#func floorcheck():
+	
 func _physics_process(delta):
+#	floorcheck()
+	if is_on_floor():
+		print("on floor")
+		velocity.y = 0
+	elif is_on_floor() == false:
+		print("not on floor")
+	#if Input.is_action_pressed(right_key):
+	#	print("right")
+	move_dir = Input.get_action_strength(right_key) - Input.get_action_strength(left_key)
+	if move_dir != 0:
+		print("moving")
+		velocity.x = move_dir*speed
+	else: 
+		velocity.x = 0
+	if Input.is_action_pressed(up_key) and is_on_floor():
+		velocity.y += jump_speed
+		
+	
 	if is_on_floor():
 		player_state = state.IDLE
 	elif not is_on_floor():
@@ -56,6 +76,7 @@ func _physics_process(delta):
 		#print("jumping")
 		JUMP()
 	move_and_slide(velocity)
+	
 
 func JUMP():
 	velocity.y += weight * gravity
