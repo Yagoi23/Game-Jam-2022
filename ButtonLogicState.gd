f340cff088b7802379a298d6b75e7213c0066d57
owewhe23 <82478297+Yagoi23@users.noreply.github.com>
2022-07-23 12:47:39 +1200

    working on channels

---
+++ ButtonLogicState.gd
@@ -0,0 +1,20 @@
+extends Node
+
+enum STATE {ON, OFF}
+
+var Channel0 = STATE.OFF
+var Channel1 = STATE.OFF
+var Channel2 = STATE.OFF
+var Channel3 = STATE.OFF
+var Channel4 = STATE.OFF
+var Channel5 = STATE.OFF
+var Channel6 = STATE.OFF
+var Channel7 = STATE.OFF
+var Channel8 = STATE.OFF
+var Channel9 = STATE.OFF
+
+#func _process(delta):
+#	if Channel0 == STATE.ON:
+#		print("channel0 = active")
+#	if Channel1 == STATE.ON:
+#		print("channel1 = active")
--- ButtonPhysics.gd
+++ ButtonPhysics.gd
@@ -1,12 +1,19 @@
 extends KinematicBody2D
+#class CustomButton:
 
 onready var raycast = $RayCast2D
+
+export var Channelid := 1
+var Channel = "Channel" + str(Channelid)
 
 export (int) var max_position_y = 1
 export (int) var max_position_x = 1
 
 export (int) var min_position_y = -1
 export (int) var min_position_x = -1
+
+enum STATE {ON, OFF}
+var button_state = STATE.OFF
 
 func _physics_process(delta):
 	if raycast.is_colliding():
@@ -15,5 +22,20 @@ func _physics_process(delta):
 	elif !raycast.is_colliding():
 		if position.y >= 0:
 			position.y -= 1
+	if position.y >= 0:
+		button_state = STATE.ON
+		#print("on")
+		ButtonLogicState.Channel0 = ButtonLogicState.STATE.ON
+		ButtonLogicState.Channel1 = ButtonLogicState.STATE.OFF
+	else:
+		button_state = STATE.OFF
+		#print("off")
+		ButtonLogicState.Channel0 = ButtonLogicState.STATE.OFF
+		ButtonLogicState.Channel1 = ButtonLogicState.STATE.ON
 	#else:
 		#print("not active")
+func Global_Activate():
+	pass
+
+func Match():
+	pass
