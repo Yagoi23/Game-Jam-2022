abcfd454d1c69115bbb1d50f5709104c70e73648
owewhe23 <82478297+Yagoi23@users.noreply.github.com>
2022-07-23 13:07:47 +1200

    Animation QuasiFinished

--- Player.gd
+++ Player.gd
@@ -21,6 +21,8 @@ var player_state = state.IDLE
 
 var move_dir
 
+signal statechanged
+
 onready var sprite = $ColorRect
 
 var right_key
@@ -28,9 +30,13 @@ var left_key
 var up_key
 
 #export (int, 0, 200) var push = 100
+func varchanged():
+	print("Boat")
+	emit_signal("statechanged")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("statechanged",$AnimationPlayer,"on_state_changed")
 	player_state = state.IDLE
 	if player_num == 1:
 		sprite.color = Color(255,255,255)
@@ -44,21 +50,29 @@ func _ready():
 #func floorcheck():
 	
 func _physics_process(delta):
-	if is_on_floor():
-		velocity.y = 0
+	var current_state = player_state
+	move_dir = Input.get_action_strength(right_key) - Input.get_action_strength(left_key)
+	if move_dir != 0:
+		velocity.x = move_dir*speed
+		#if move_dir == 1:
+		#	scale.x = 2
+		#elif move_dir == -1:
+		#	scale.x = -2
+		if is_on_floor():
+			velocity.y = 0
+			player_state = state.RUNNING
+			
+	else:
+		velocity.x = 0
 		player_state = state.IDLE
-	elif !is_on_floor():
+	if !is_on_floor():
 		if velocity.y <= 0:
 			player_state = state.JUMP
 			JUMP()
 		else:
 			player_state = state.FALL
 			FALL()
-	move_dir = Input.get_action_strength(right_key) - Input.get_action_strength(left_key)
-	if move_dir != 0:
-		velocity.x = move_dir*speed
-	else:
-		velocity.x = 0
+
 	if Input.is_action_pressed(up_key) and is_on_floor():
 		velocity.y += jump_speed
 	move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
@@ -72,6 +86,13 @@ func _physics_process(delta):
 		var collision = get_slide_collision(index)
 		if collision.collider.is_in_group("entity"):
 			collision.collider.apply_central_impulse(-collision.normal * push)
+	animation()
+	if current_state != player_state:
+		varchanged()
+func animation():
+	#$AnimationPlayer.play(str(player_state))
+	
+	print(player_state)
 	
 
 func JUMP():
