1002ecf9dc60d402ad08b0b6922032c2a946d33c
owewhe23 <petwoo10@kingshigh.school.nz>
2022-07-24 10:18:08 +1200

    Minor changes

--- AutoPlatform.gd
+++ AutoPlatform.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 export (int) var speed = 10
-export (int) var rot = 10
-export (bool) var rotating
+#export (int) var rot = 10
+export (bool) var reverse
+export (bool) var activatable
 var velocity = Vector2()
 export (int) var Channelid = 0
 var dir = 1
@@ -13,14 +14,19 @@ func _ready():
 	base_y = position.y
 
 func _physics_process(delta): #makes a block that moves horizontaly and changes direction when it hits a wall
-	velocity.x = speed * dir
+	#velocity.x = speed * dir
 	position.y = base_y
-	velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
+	#velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
 	if is_on_wall():
 		dir = dir * -1
 	var active = check_active()
-	if rotating == true and active == false:
-		rotation_degrees = rotation_degrees + rot
+	if activatable == true:
+		if active != reverse:
+			velocity.x = speed * dir
+			velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
+	else:
+		velocity.x = speed * dir
+		velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
 	
 func check_active():
 	if Channelid == 0:
